b777144514c4d30db77cf631cb6145c41ae44bff
quy_tb(VN) <66452165+NSVN-tranbaquy@users.noreply.github.com>
2024-03-01 10:05:23 +0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String[] args){
-		for(int i = 1; i< 10; i++){
-			System.out.println("HelloWorld ..." + i + " from git");
+		for(int j = 1; j< 10; j++){
+			System.out.println("HelloWorld ..." + j + " from git");
 		}
 	}
 }
