25f994a1de115ced26ca1aca4f54196df494f603
quy-tb(VN) <quy-tb@kinjiro-vn.com>
2024-03-01 10:10:50 +0700

    commit 1 from test branch

--- Hello.java
+++ Hello.java
@@ -2,10 +2,11 @@ public class Hello {
 	public static void main(String[] args){
 		for(int j = 1; j< 10; j++){
 			System.out.println("HelloWorld ..." + j + " from git");
-		}
+		}	
 		
-		for(int i = 1; i< 10; i++){
-			System.out.println("HelloWorld ..." + i + " from git");
-		}
 	}
-}
+	
+	private void RandomNumber (){
+		//TODO viet code vao day
+		System.out.println("HelloWorld ..." );
+	}
